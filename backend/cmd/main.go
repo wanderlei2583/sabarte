c91e307447c2b911d720462a57d0cebd33aeba45
Wanderlei <devwanderlei@gmail.com>
2025-01-04 15:12:07 -0300

    initial commit do backend

---
+++ backend/cmd/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"context"
+	"log"
+	"time"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+func connectDB() *mongo.Client {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	client, err := mongo.Connect(
+		ctx,
+		options.Client().ApplyURI("mongodb://localhost:27017"),
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Verifica se a conexão foi estabelecida com sucesso
+	if err := client.Ping(ctx, nil); err != nil {
+		log.Fatal(err)
+	}
+
+	log.Println("Conectado ao MongoDB!")
+
+	return client
+}
+
+func main() {
+	client := connectDB()
+	defer client.Disconnect(context.Background())
+}
