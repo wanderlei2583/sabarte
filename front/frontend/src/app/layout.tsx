1f584402a866c876bce9767e27c1903343617b4a
Wanderlei <devwanderlei@gmail.com>
2025-01-08 17:41:44 -0300

    mouse artistico

--- front/frontend/src/app/layout.tsx
+++ front/frontend/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import ArtisticMouse from "./components/artisticmouse";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,7 +28,12 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        {/* Mouse-Pincel Artistico */}
+        <ArtisticMouse />
+        {/* Conteúdo Principal */}
+        <div style={{ position: "relative", zIndex: 1 }}>
+          {children}
+        </div>
       </body>
     </html>
   );
